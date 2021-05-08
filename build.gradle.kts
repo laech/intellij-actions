4020d68ddfeca20a321b82d5310651e286891bae
Lae Chen <laec@protonmail.ch>
2021-05-08 21:59:34 +1200

    Use build.gradle.kts.

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.jetbrains.kotlin.jvm").version("1.3.72")
+  id("org.jetbrains.intellij").version("0.7.3")
+}
+
+group = "com.gitlab.lae.intellij.actions"
+version = "0.6.1-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+listOf("compileKotlin", "compileTestKotlin").forEach {
+  tasks.getByName<KotlinCompile>(it) {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+}
+
+tasks.getByName<Test>("test") {
+  testLogging {
+    exceptionFormat = TestExceptionFormat.FULL
+  }
+}
+
+intellij(Action {
+  updateSinceUntilBuild = false
+  version =
+    project.properties.getOrDefault("intellijVersion", "2019.1").toString()
+})
